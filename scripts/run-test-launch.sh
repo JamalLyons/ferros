e5ecd5af310badd0d934c633d0280ac72885ac1c
Jamal <43446907+JamalLyons@users.noreply.github.com>
2025-11-19 19:12:20 -0500

    Create run-test-launch.sh

---
+++ scripts/run-test-launch.sh
@@ -0,0 +1,5 @@
+sudo cargo build --example test_target
+sudo cargo build --example test_target
+
+# sudo cargo run -- launch target/debug/examples/simple_test --log-level debug
+sudo cargo run -- launch target/debug/examples/test_target --log-level debug

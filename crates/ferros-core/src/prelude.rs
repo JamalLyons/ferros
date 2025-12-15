75cd38ecdf449ec7a871a18354d6e05add7c3274
Jamal <43446907+JamalLyons@users.noreply.github.com>
2025-12-14 21:18:27 -0500

    Update prelude.rs

--- crates/ferros-core/src/prelude.rs
+++ crates/ferros-core/src/prelude.rs
@@ -7,6 +7,7 @@ pub use ferros_utils::{debug, info, trace, warn};
 
 pub use crate::debugger::{FerrosDebugger, create_debugger};
 pub use crate::error::{FerrosError, FerrosResult};
+#[cfg(target_os = "macos")]
 pub use crate::platform::macos::*;
 pub use crate::types::address::Address;
 pub use crate::types::process::{Architecture, MemoryRegion, MemoryRegionId, ProcessId, StopReason, ThreadId};
